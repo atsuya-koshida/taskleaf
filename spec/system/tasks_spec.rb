6e72d6712f9a81ddc7a3c85c01381de54d04dd37
atsuyakoshida <koshi0845@gmail.com>
2020-03-17 19:21:42 +0900

    add test tasks#show

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,31 +1,46 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
+
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    it { expect(page).to have_content '最初のタスク' }
+  end
+
   describe '一覧表示機能' do
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
-
-    before do
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a) # この時点でユーザーAが実際にDBに登録される。作成者がユーザーAであるタスクを作成しておく。
-      visit login_path  # ユーザーAでログインする
-      fill_in 'メールアドレス', with: login_user.email # メールアドレスを入力する
-      fill_in 'パスワード', with: login_user.password         # パスワードを入力する
-      click_button 'ログインする'                    # ログインするボタンを押す
-    end
-
     context 'ユーザーAがログインしている時' do
       let(:login_user) { user_a }
-
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'  # 作成済みのタスクの名称が画面上に表示されている事を確認
-      end
+      
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
 
     context 'ユーザーBがログインしている時' do
       let(:login_user) { user_b }
+      
       it 'ユーザーAが作成したタスクが表示されない' do
-        expect(page).to have_no_content '最初のタスク' # ユーザーAが作成したタスクの名称が画面上に表示されていない事を確認
+        expect(page).to have_no_content '最初のタスク'
       end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしている時' do
+      let (:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
   end
 end
