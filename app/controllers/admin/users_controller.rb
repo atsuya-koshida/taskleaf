25840c4e519e98e6070217dc531d84431bc5bf5f
atsuyakoshida <koshi0845@gmail.com>
2020-03-17 12:48:21 +0900

    create users views

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,48 @@
 class Admin::UsersController < ApplicationController
-  def new
-  end
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  def edit
+  def index
+    @users = User.all
   end
 
   def show
   end
 
-  def index
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path(@user), notice: "ユーザー「#{@user.name}」を更新しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー「#{@user.name}」を削除しました。"
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
