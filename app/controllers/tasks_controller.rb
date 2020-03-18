c3a095618520ad4884e057748da2217f7c58edd7
atsuyakoshida <koshi0845@gmail.com>
2020-03-18 21:36:10 +0900

    Ajax tasks#destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,7 +48,7 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
+    head :no_content    #headメソッドを使いHTTPステータス204(no_content)を返し、成功と判定する
   end
 
   def import
