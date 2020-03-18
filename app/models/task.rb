ba4de4201b0b48b1ed11d1ae99a390b539bb00b5
atsuyakoshida <koshi0845@gmail.com>
2020-03-18 16:20:53 +0900

    add csv export function

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,6 +8,19 @@ class Task < ApplicationRecord
 
   scope :recent, -> { order(created_at: :desc) }
 
+  def self.csv_attributes
+    ["name", "description", "created_at", "updated_at"]   #CSVデータにどの属性をどの順番で出力するかをcsv_attributesというクラスメソッドから得られるように定義している
+  end
+
+  def self.generate_csv
+    CSV.generate(headers: true) do |csv|    #CSV.generateを使ってCSVデータの文字列を生成。生成した文字列がgenerate_csvクラスメソッドの戻り値となる。
+      csv << csv_attributes                 #CSVの1行目としてヘッダを出力する。
+      all.each do |task|
+        csv << csv_attributes.map{ |attr| task.send(attr) }   #allメソッドで全タスクを取得。1レコード毎にCSVの1行を出力する。
+      end
+    end
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     %w[name created_at]
   end
